aaa71b922004109ac0cfbcdcee32fbe68c7c526b
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-06-29 03:32:49 +0700

    update error handling using new error handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,43 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
-
-	"github.com/ynrfin/greenlight/internal/data"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+    // Declare an anonymous struct to held the information that we expect to be in the
+    // HTTP request body (note that the field names and types in the struct are a subset
+    // of the Movie struct that we created earlier). This struct will be our *target
+    // decode destination*
+    var input struct {
+        Title string `json:"title"`
+        Year int32 `json:"year"`
+        Runtime int32 `json:"runtime"`
+        Genres []string `json:"genres"`
+    }
+
+    // Initialize a new json.Decoder instance which reads from the request body, and
+    // the use the Decode() method  to decode the body contents into the input struct.
+    // Importantly, notice that when we call Decode() we bass a *pointer* to the input
+    // struct as the traget decode destination. If there was an error during decoding,
+    // we also use our generic errorResponse() helper to send the clien ta 400 bad
+    // request response containing the error message.
+    err := app.readJSON(w, r, &input)
+
+    if err != nil {
+        app.badRequestResponse(w, r, err)
+        return
+    }
+    // Dump the contents of the input struct in a HTTP response.
+    fmt.Fprintf(w, "%+v\n", input)
 }
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
 // the interpolated "id" parameter from the current URL and include it in a placeholder
 // response.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIdParam(r)
-	if err != nil {
-		app.notFoundResponse(w, r)
-		return
-	}
+    // Declare an anonymous struct to held the information that we expect to be in the
+    // HTTP request body (note that the field names and types in the struct are a subset
+    // of the Movie struct that we created earlier). This struct will be our *target
+    // decode destination*
+    var input struct {
+        Title string `json:"title"`
+        Year int32 `json:"year"`
+        Runtime int32 `json:"runtime"`
+        Genres []string `json:"genres"`
+    }
 
-	// Create a nwe instance of the Movie struct,containing the ID we extracted from
-	// the URL and some dummy data. Also notice tha twe deliberately haven't set a
-	// value for the Year field
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   182,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
-	}
+    // Initialize a new json.Decoder instance which reads from the request body, and
+    // the use the Decode() method  to decode the body contents into the input struct.
+    // Importantly, notice that when we call Decode() we bass a *pointer* to the input
+    // struct as the traget decode destination. If there was an error during decoding,
+    // we also use our generic errorResponse() helper to send the clien ta 400 bad
+    // request response containing the error message.
+    err := json.NewDecoder(r.Body).Decode(&input)
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-	}
+    if err != nil {
+        app.errorResponse(w, r, http.StatusBadRequest, err.Error)
+        return
+    }
+    // Dump the contents of the input struct in a HTTP response.
+    fmt.Fprintf(w, "%+v\n", input)
 }
