a4193068ca886a5e21e2d9b85c8c04e1ef1b7209
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-06-29 04:18:31 +0700

    use Runtime

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/ynrfin/greenlight/internal/data"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
@@ -16,7 +18,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
     var input struct {
         Title string `json:"title"`
         Year int32 `json:"year"`
-        Runtime int32 `json:"runtime"`
+        Runtime data.Runtime `json:"runtime"`
         Genres []string `json:"genres"`
     }
 
