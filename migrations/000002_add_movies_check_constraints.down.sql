616dc0a74b2fa614faadb9f725395f90a8feadc2
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-07-21 04:31:01 +0700

    feat: create migration for movies table

---
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE movies DROP constraint IF EXISTS movies_rutime_check;
+ALTER TABLE movies DROP constraint IF EXISTS movies_year_check;
+ALTER TABLE movies DROP constraint IF EXISTS genres_length_check;
