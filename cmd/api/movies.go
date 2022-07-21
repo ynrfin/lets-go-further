1e1e9321ab7f665ee72dee3a31ac53b1007bf3f9
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-07-22 05:59:46 +0700

    feat: update insert movie handler to really insert data to database using new insert function

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -28,6 +28,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
         app.badRequestResponse(w, r, err)
     }
 
+    // Note that the mevoe variable contains a *pointer* to a Movie struct
     movie := &data.Movie{
         Title: input.Title,
         Year: input.Year,
@@ -42,20 +43,28 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
         return
     }
 
-    // Initialize a new json.Decoder instance which reads from the request body, and
-    // the use the Decode() method  to decode the body contents into the input struct.
-    // Importantly, notice that when we call Decode() we bass a *pointer* to the input
-    // struct as the traget decode destination. If there was an error during decoding,
-    // we also use our generic errorResponse() helper to send the clien ta 400 bad
-    // request response containing the error message.
-    err = app.readJSON(w, r, &input)
-
+    // Call the Insert() method on our movies model, passing in a pointer to the
+    // validated movie struct. This will create a record in the database and update the
+    // movie struct with the system-genrated information.
+    err = app.models.Movies.Insert(movie)
     if err != nil {
-        app.badRequestResponse(w, r, err)
+        app.serverErrorResponse(w, r, err)
         return
     }
-    // Dump the contents of the input struct in a HTTP response.
-    fmt.Fprintf(w, "%+v\n", input)
+
+    // When sending a HTTP respones, we want to include a Location header to let the
+    // client know which URL they can find the newly-created resource at. We make an
+    // empty http.Header map and then use the Set() method to add a new Location header.
+    // interpolating the system-generated ID for our new movie in the URL.
+    headers := make(http.Header)
+    headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+    // Write a JSON response with 201 Created status code. the movie data in the
+    // response body, and the Location header.
+    err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+    if err != nil{
+        app.serverErrorResponse(w, r, err)
+    }
 }
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
