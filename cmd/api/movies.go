58362006f9eaa63554ffce2d19805f7afb8eddcc
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-07-27 04:24:49 +0700

    ch7.3: can get movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -71,29 +71,29 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // the interpolated "id" parameter from the current URL and include it in a placeholder
 // response.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-    // Declare an anonymous struct to held the information that we expect to be in the
-    // HTTP request body (note that the field names and types in the struct are a subset
-    // of the Movie struct that we created earlier). This struct will be our *target
-    // decode destination*
-    var input struct {
-        Title string `json:"title"`
-        Year int32 `json:"year"`
-        Runtime int32 `json:"runtime"`
-        Genres []string `json:"genres"`
-    }
-
-    // Initialize a new json.Decoder instance which reads from the request body, and
-    // the use the Decode() method  to decode the body contents into the input struct.
-    // Importantly, notice that when we call Decode() we bass a *pointer* to the input
-    // struct as the traget decode destination. If there was an error during decoding,
-    // we also use our generic errorResponse() helper to send the clien ta 400 bad
-    // request response containing the error message.
-    err := json.NewDecoder(r.Body).Decode(&input)
-
+    id, err := app.readIdParam(r)
     if err != nil {
-        app.errorResponse(w, r, http.StatusBadRequest, err.Error)
+        app.notFoundResponse(w, r)
         return
     }
-    // Dump the contents of the input struct in a HTTP response.
-    fmt.Fprintf(w, "%+v\n", input)
+
+    // Call the Get() method to fetch the data for a specific movie. We also need to
+    // use the errors.Is() function to check if it returns a data.ErrorRecordNotFound
+    // error, in which case we send a 404 Not Found response to the client.
+    movie, err := app.models.Movies.Get(id)
+    if err != nil {
+        switch  {
+        case errors.Is(err, data.ErrRecordNotFound):
+            app.notFoundResponse(w, r)
+        default:
+            app.serverErrorResponse(w, r, err)
+        }
+        return
+    }
+
+    err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+    }
 }
