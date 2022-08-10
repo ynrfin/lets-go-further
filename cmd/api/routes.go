7dd695c32753f0c067736c4f21b7b37daddb51dd
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-08-10 21:51:11 +0700

    ch 13.3 routing to create user

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -29,6 +29,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+    // Add the route for the POST /v1/users endpoint
+    router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 
 }
