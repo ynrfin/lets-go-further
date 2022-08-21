5307dcde54771c381fd90e318abfaacd1c243403
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2022-08-21 20:46:12 +0700

    15.3 Sending Activation Token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/ynrfin/greenlight/internal/data"
 	"github.com/ynrfin/greenlight/internal/validator"
@@ -66,23 +67,32 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// Write a JSON response containing the user data along with a 201 Created status
-	// code.
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	// After the user record has been created in the database,generate a new activation
+	// token for the user
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
+		return
 	}
 
 	// launch go routine which runs an anonymous function that sends the welcome email.
-    app.background(func(){
+	app.background(func() {
+		// As there are now multiple pieces of data that we want to pass to our email
+		// templates, we create a map to act as a 'holding structure' for the data. This
+		// contains the plaintext version of the tactivation toke nfor the user, along
+		// with their id
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
 
 		// Call the Send() method on our Mailer, passing in the user's email addres,
 		// name of the template file, and the User struct containing the new user's data.
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintErr(err, nil)
 		}
-    })
+	})
 
 	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
 	if err != nil {
